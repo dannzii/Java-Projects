854c46d639c0dfd2b037547ca7c978b21147fda0
dannzii <34076867+dannzii@users.noreply.github.com>
2018-01-25 14:47:15 -0500

    Add files via upload

---
+++ Myfirstcode.java
@@ -0,0 +1,11 @@
+public class Myfirstcode
+{
+ 		public static void main (String[] args)
+ 		{
+ 			int d=10;
+ 			int p=20;
+ 			int r=d+p;
+ 			System.out.println("this is my reult:"+r);
+ 		}
+
+}
